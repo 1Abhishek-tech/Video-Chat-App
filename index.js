96506520fe70d71559ee8d28f20f3f418c8085ca
Abhishek Agnihotri <abhishekagni1234@gmail.com>
2022-08-26 14:47:10 +0530

    setting up room via uuid

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-
+const { v4: uuidv4 } = require('uuid');
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.redirect(`/${uuidv4()}`);
 });
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
 
 server.listen(8000, () => {
     console.log('Server started on port 3000');
